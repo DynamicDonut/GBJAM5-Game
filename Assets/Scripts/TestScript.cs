9c4a5d91b02ff29f7fc66100ef47f344534bb5fa
DynamicDonut <bwilldonut@gmail.com>
2016-10-06 04:27:39 -0400

    Initial Push

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestScript : MonoBehaviour {
+
+	public float spd;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Vector3.forward * Time.deltaTime * spd);
+	}
+}

175f1306ffce31c02d6ff536448e0523940bb0ab
DynamicDonut <bwilldonut@gmail.com>
2016-10-07 02:16:59 -0400

    Initial Scripting and Sprite implementation
    Time dial movement set up, beginnings of customer spawning.

---
+++ Assets/Scripts/GM.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GM : MonoBehaviour {
+
+	public float CustomerSpawnTime;
+	float last_CSTime = 0f;
+
+	public Transform CustomersList;
+	int CustomerCount;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time >= last_CSTime) {
+			CustomerCount++;
+			for (int i = 0; i < CustomersList.childCount; i++) {
+				CustomersList.GetChild (i).gameObject.SetActive (false);
+			}
+			CustomersList.GetChild(Random.Range(0, CustomersList.childCount)).gameObject.SetActive(true);
+			last_CSTime += CustomerSpawnTime;
+		}
+	}
+}
---
+++ Assets/Scripts/SunDialScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SunDialScript : MonoBehaviour {
+	
+	Vector3 sunPosition;
+	float currTime;
+	Transform sunIcon;
+
+	[Range(0f, 2.0f)]
+	public float gameTime;
+	public float maxGameTime;
+
+	// Use this for initialization
+	void Start () {
+		gameTime = 0f;
+		sunIcon = transform.FindChild ("Sun-Icon");
+		sunPosition = sunIcon.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currTime = Mathf.Clamp (Time.time, 0f, 60f);
+		gameTime = mapVal (currTime, 0f, 60f, 0f, 2f);
+
+		sunPosition.x = (gameTime - 1f) * 69;
+		sunIcon.position = sunPosition;
+	}
+
+	public static float mapVal(float value, float lMin, float lMax, float rMin, float rMax){
+		return rMin + (value - lMin) * (rMax - rMin) / (lMax - lMin);
+	}
+}
